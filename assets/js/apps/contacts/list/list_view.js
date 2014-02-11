7488941f5eb21e530423a8de6da13f724ad40ae7
John Betley <jpbetley@gmail.com>
2014-02-11 14:28:17 -0500

    composite view for table with heading

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,13 +1,16 @@
 ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbone, Marionette, $, _) {
     
     List.Contact = Marionette.ItemView.extend({
-        tagName: 'li',
-        template: _.template(List.Templates.listItemView)
+        tagName: 'tr',
+        template: '#contact-list-item'
     });
 
-    List.Contacts = Marionette.CollectionView.extend({
-        tagName: "ul",
-        itemView: List.Contact
+    List.Contacts = Marionette.CompositeView.extend({
+        tagName: "table",
+        className: "table table-hover",
+        template: '#contact-list',
+        itemView: List.Contact,
+        itemViewContainer: 'tbody'
     });
 
 });
