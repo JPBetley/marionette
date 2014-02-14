c531ed7a8caeba215892ae6ddbb7f4df08fd9c73
John Phillip Betley <jpbetley@gmail.com>
2014-02-13 19:49:05 -0500

    updated validation to clear before applying

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -14,13 +14,30 @@ ContactManager.module("ContactsApp.Edit", function (Edit, ContactManager, Backbo
 
         onFormDataInvalid: function (errors) {
             var self = this;
+            var $view = this.$el;
+            
+            var clearFormErrors = function () {
+                var $form = $view.find("form");
+                $form.find("span.form-control-feedback").each(function() {
+                    $(this).remove();
+                });
+                $form.find("div.help-block").each(function () {
+                    $(this).remove();
+                });
+                $form.find(".form-group").each(function () {
+                    $(this).removeClass("has-error").removeClass("has-feedback");
+                });
+            };
+
             var markErrors = function (value, key) {
-                var $formControl = self.$el.find("#contact-" + key);
+                var $formControl = $view.find("#contact-" + key);
                 var $formGroup = $formControl.parent();
                 var $errorIcon = $("<span>", { class: "glyphicon glyphicon-remove form-control-feedback" });
                 var $errorEl = $("<div>", { class: "help-block", text: value });
                 $formGroup.append($errorIcon).append($errorEl).addClass("has-error").addClass("has-feedback");
             }
+
+            clearFormErrors();
             _.each(errors, markErrors);
         }
     });
