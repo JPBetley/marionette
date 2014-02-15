a5d0a58408e59ec3325dca863ace4d0c51bc0ae2
John Phillip Betley <jpbetley@gmail.com>
2014-02-15 16:05:44 -0500

    add layout added. no functionality

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -10,7 +10,7 @@ ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbo
     });
 
     List.Panel = Marionette.ItemView.extend({
-        template: '#contacts-list-panel'
+        template: '#contact-list-panel'
     });
 
     List.Contact = Marionette.ItemView.extend({
