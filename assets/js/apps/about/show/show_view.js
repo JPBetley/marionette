013bb313462bc23669f5f6fecb33e07eb7aebba1
John Betley <jpbetley@gmail.com>
2014-02-20 15:00:03 -0500

    About app

---
+++ assets/js/apps/about/show/show_view.js
@@ -0,0 +1,5 @@
+﻿ContactManager.module("AboutApp.Show", function (Show, ContactManager, Backbone, Marionette, $, _) {
+	Show.Message = Marionette.ItemView.extend({
+		template: '#about-message'
+	});
+});
