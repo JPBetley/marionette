017f157ec4ea35b49b799e6c2595b628a45aa591
John Betley <jpbetley@gmail.com>
2014-02-20 16:26:21 -0500

    header functionality. Need to work in events

---
+++ assets/js/apps/header/header_app.js
@@ -0,0 +1,15 @@
+﻿ContactManager.module("HeaderApp", function (Header, ContactManager, Backbone, Marionette, $, _) {
+    var API = {
+        listHeader: function () {
+            Header.List.Controller.listHeader();
+        }
+    };
+
+    ContactManager.commands.setHandler("set:active:header", function (name) {
+        ContactManager.Header.List.Controller.setActiveHeader(name);
+    });
+
+    Header.on("start", function () {
+        API.listHeader();
+    });
+});
