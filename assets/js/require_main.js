92f21ad645bae18b70c993784bdb36feaf665968
John Betley <jpbetley@gmail.com>
2014-02-26 12:34:33 -0500

    require js starting

---
+++ assets/js/require_main.js
@@ -0,0 +1,30 @@
+﻿requirejs.config({
+    baseUrl: "assets/js",
+    paths: {
+        jquery: "vendor/jquery",
+        underscore: "vendor/underscore",
+        json2: "vendor/json2",
+        backbone: "vendor/backbone",
+        marionette: "vendor/backbone.marionette"
+    },
+
+    shim: {
+        underscore: {
+            exports: "_"
+        },
+        backbone: {
+            deps: ['jquery', 'underscore', 'json2'],
+            exports: "Backbone"
+        },
+        marionette: {
+            deps: ['backbone'],
+            exports: "Marionette"
+        }
+    }
+});
+
+require(['backbone'], function (bb) {
+    console.log('jQuery version: ', $.fn.jquery);
+    console.log('underscore identity call: ', _.identity(5));
+    console.log('Backbone history: ', bb.history);
+});
